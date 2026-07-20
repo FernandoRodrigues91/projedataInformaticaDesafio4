ae49b18394a766a4a8af1f98b5fe1148aa2dfebf
Fernando <jk157_372@hotmail.com>
2026-07-20 00:04:28 -0300

    configurando o jest

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import { Config } from 'jest';
+
+const config: Config = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+};
+
+export default config;
